8e1d3edf53888fcb63ca1baa7761028e5dbd9716
Dustin Heisey <dustin@heisey.email>
2024-09-03 11:57:36 +0200

    feat: split slider

---
+++ site/scripts/funcs/split-slider.js
@@ -0,0 +1,10 @@
+export function splitSlider() {
+  const container = document.querySelector('.container')
+  document.querySelector('.slider-button').addEventListener('click', (e) => {
+    console.log('sliderbutton')
+    container.style.setProperty('--position', `${e.target.value}%`)
+  })
+  document.querySelector('.slider').addEventListener('input', (e) => {
+    container.style.setProperty('--position', `${e.target.value}%`)
+  })
+}
--- site/scripts/index.js
+++ site/scripts/index.js
@@ -2,6 +2,7 @@ import { readingProgress } from './funcs/reading-progress.js'
 import { scrollingNav } from './funcs/scrolling-nav.js'
 import { range } from './funcs/range.js'
 import { darkMode } from './funcs/dark-mode.js'
+import { splitSlider } from './funcs/split-slider.js' // Import the split slider function
 
 document.addEventListener('DOMContentLoaded', function () {
   const navbar = document.querySelector('#navbar')
@@ -13,4 +14,7 @@ document.addEventListener('DOMContentLoaded', function () {
   if (budgetInput) range()
   if (progress) readingProgress()
   if (navbar) scrollingNav()
+
+  // Initialize the split slider
+  splitSlider()
 })
