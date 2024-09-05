23fd9b312d2bdb0709ba9150c3254b9c151f0498
Dustin Heisey <dustin@heisey.email>
2024-09-05 16:58:34 +0200

    chore: eslint flat config

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+// ? https://allalmohamedlamine.medium.com/flat-config-setting-up-eslint-and-prettier-with-eslint-prettier-plugin-7bd1ca453129
+
+import prettierPlugin from 'eslint-plugin-prettier'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import globals from 'globals'
+
+export default [
+  {
+    files: ['**/*.{mjs,cjs,js}'],
+    languageOptions: {
+      globals: {
+        ...globals.es2021
+      }
+      // ecmascriptVersion, and sourceType, default is right
+    },
+    plugins: {
+      prettier: prettierPlugin
+    },
+    rules: {
+      quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: false }],
+      // 'import/no-cycle': 'warn',
+      'class-methods-use-this': 'off',
+      // 'import/prefer-default-export': 'off',
+      'comma-dangle': 'off',
+      'object-curly-newline': 'off',
+      'operator-linebreak': 'off',
+      'implicit-arrow-linebreak': 'off',
+      'function-paren-newline': 'off',
+      'import/no-extraneous-dependencies': 'off',
+      'import/extensions': 'off',
+      'import/no-absolute-path': 'off',
+      'generator-star-spacing': 'off',
+      'no-prototype-builtins': 'off',
+      'no-underscore-dangle': 'off',
+      'no-plusplus': 'off',
+      'no-undef': 'warn',
+      'no-case-declarations': 'warn',
+      ...prettierPlugin.configs.recommended.rules,
+      ...eslintConfigPrettier.rules
+    },
+    ignores: [
+      'site/_site/**/*.js',
+      '!.storybook',
+      'index.min.js',
+      'wiki/build/**/*.js',
+      'wiki/.docusaurus/**.js',
+      'wiki/babel.config.js'
+    ]
+  }
+]
