08df84f355e3ea6e6d63544598078fb3b0b7d1fe
Dustin Heisey <dustin@heisey.email>
2024-09-10 11:11:06 +0200

    fix: update baseUrl

--- wiki/docusaurus.config.js
+++ wiki/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://inconvenient.gg',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/wiki',
+  baseUrl: '/',
   staticDirectories: ['../common/icons', '../site/public/svg'],
   i18n: {
     defaultLocale: 'en',
